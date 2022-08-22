e7036a8f6f16942d6f6f14728d80ab7a605d7824
Sam Arita <arita.sam@student.moringaschool.com>
2022-08-23 01:21:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function moveDodgerRight(){
     const left = parseInt(leftNumbers, 10)
     // const rightNumbers = dodger.style.right.replace('px', '')
     // const right = parseInt(rightNumbers, 10)
-    if (left >= 0){
+    if (left < 360){
         dodger.style.left = `${left +1}px`
     }
 }
